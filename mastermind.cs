8efe4e713b9bc7b4ec658b40234cf54c9e8dc7e5
Michael Leo <mleo2857@gmail.com>
2019-11-19 21:11:03 -0500

    Made some outputs look better and added failures message

--- mastermind.cs
+++ mastermind.cs
@@ -4,7 +4,7 @@ class Mastermind {
   static void Main(string[] args) {
     Random random = new Random();
 
-    Console.WriteLine("Welcome to Mastermind");
+    Console.WriteLine("Welcome to Mastermind, you will have 10 attempts to guess my code");
 
     int[] code = new int[4];
     for (int i = 0; i < code.Length; i++){
@@ -17,7 +17,7 @@ class Mastermind {
     
     while (attempt < 10){
         
-        Console.WriteLine("Try to guess my 4 digit code (Numbers between 1 and 6)");
+        Console.WriteLine("Try to guess my 4 digit code (Numbers between 1 and 6) Attempt: " + (attempt + 1));
         string guess = Console.ReadLine().Replace(" ","");
 
         int[] guessArray = new int[4];
@@ -38,17 +38,20 @@ class Mastermind {
             }
         }
         
-        string outputString = String.Join("",outputStringArray);
-        if (outputString == "++++"){
+        string outputString = String.Join(" ",outputStringArray);
+        if (outputString == "+ + + +"){
             Console.WriteLine("You have cracked the code!! You Win!!!!");
             break;
         } else {
             Console.WriteLine("'+' is correct number & position, '-' is correct number wrong position, ' ' is an incorrect number");
             Console.WriteLine(outputString);
+            Console.WriteLine("- - - -");
             attempt++;
         }
 
     }
+
+    Console.WriteLine("You have run out of guesses, the code was " + code[0] + code[1] + code[2] + code[3]);
     
   }
 }
