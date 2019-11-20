dd69778e209b8cc4e46970d58ce61722b907837e
Michael Leo <mleo2857@gmail.com>
2019-11-19 20:57:29 -0500

    Game is functional

--- mastermind.cs
+++ mastermind.cs
@@ -2,7 +2,53 @@ using System;
 
 class Mastermind {
   static void Main(string[] args) {
-    int[] code = {4,7,2,9};
-    Console.WriteLine(Array.Exists(code, element => element == 8));
+    Random random = new Random();
+
+    Console.WriteLine("Welcome to Mastermind");
+
+    int[] code = new int[4];
+    for (int i = 0; i < code.Length; i++){
+        int randomNumber = random.Next(1,7);
+        code[i] = randomNumber;
+    }
+
+
+    int attempt = 0;
+    
+    while (attempt < 10){
+        
+        Console.WriteLine("Try to guess my 4 digit code (Numbers between 1 and 6)");
+        string guess = Console.ReadLine().Replace(" ","");
+
+        int[] guessArray = new int[4];
+        for (int i = 0; i < guess.Length; i++){
+            int guessNumber = (int)Char.GetNumericValue(guess[i]);
+            guessArray[i] = guessNumber;
+        }
+
+        string[] outputStringArray = new string[4];
+
+        for (int i = 0; i < guessArray.Length; i++){
+            if (guessArray[i] == code[i]){
+                outputStringArray[i] = "+";
+            } else if (Array.Exists(code, element => element == guessArray[i])){
+                outputStringArray[i] = "-";
+            } else {
+                outputStringArray[i] = " ";
+            }
+        }
+        
+        string outputString = String.Join("",outputStringArray);
+        if (outputString == "++++"){
+            Console.WriteLine("You have cracked the code!! You Win!!!!");
+            break;
+        } else {
+            Console.WriteLine("'+' is correct number & position, '-' is correct number wrong position, ' ' is an incorrect number");
+            Console.WriteLine(outputString);
+            attempt++;
+        }
+
+    }
+    
   }
 }
